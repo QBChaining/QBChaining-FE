b9001bf9e831285a0d1b2d6ecfc5fca50785cbb7
Changsoon-YUN <changsoonyun@naver.com>
2022-09-18 20:59:35 +0900

    Nodata type추가

--- src/components/bookmark/Nodata.jsx
+++ src/components/bookmark/Nodata.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import BookmarkFillIcon from "../../assets/images/BookmarkFillIcon.png";
 
-const Nodata = () => {
+const Nodata = ({ type }) => {
   return (
     <SNodata>
       <SIcon src={BookmarkFillIcon} />
