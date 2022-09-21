40f9e6efd797880c62399bd4efc917c14ebb6a45
Changsoon-YUN <changsoonyun@naver.com>
2022-09-22 05:37:58 +0900

    style/mypageCube

--- src/pages/myPage/MyPage.jsx
+++ src/pages/myPage/MyPage.jsx
@@ -13,6 +13,24 @@ const MyPage = () => {
     // dispatch(getUserInfoActivityDB());
   }, []);
 
+  const week1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+
+  const week2 = [
+    [11, 12, 13],
+    [14, 15, 16],
+    [17, 18, 19],
+  ];
+
+  const week3 = [
+    [21, 22, 23],
+    [24, 25, 26],
+    [27, 28, 29],
+  ];
+
   return (
     <SMyPage>
       <button
@@ -90,7 +108,37 @@ const MyPage = () => {
           </SBlog>
         </SList>
       </SListWrapper>
-      <SCube></SCube>
+      <SCube>
+        <SCubeWrapper>
+          {week1.map((data, i) => (
+            <div key={i}>
+              {data.map((data2, i) => (
+                <div key={i}></div>
+              ))}
+            </div>
+          ))}
+        </SCubeWrapper>
+        <SCubeWrapper>
+          {week2.map((data, i) => (
+            <div key={i}>
+              {" "}
+              {data.map((data2, i) => (
+                <div key={i}></div>
+              ))}
+            </div>
+          ))}
+        </SCubeWrapper>
+        <SCubeWrapper>
+          {week3.map((data, i) => (
+            <div key={i}>
+              {" "}
+              {data.map((data2, i) => (
+                <div key={i}></div>
+              ))}
+            </div>
+          ))}
+        </SCubeWrapper>
+      </SCube>
     </SMyPage>
   );
 };
@@ -195,4 +243,49 @@ const SListDate = styled.div`
   color: ${props => props.theme.color.grey5};
 `;
 
-const SCube = styled.div``;
+const SCube = styled.div`
+  position: absolute;
+  top: 0;
+  right: 200px;
+  display: flex;
+`;
+const SCubeWrapper = styled.div`
+  display: flex;
+  position: relative;
+  transition: 2s;
+
+  & > div > div {
+    width: 30px;
+    height: 30px;
+    margin: 3px;
+    background-color: ${props => props.theme.color.mainGreen};
+  }
+  /* 
+  &:first-child {
+    transform: rotate(120deg) skew(0deg, -30deg);
+  }
+
+  &:nth-child(2) {
+    transform: rotate(-120deg) skew(0deg, 30deg);
+  }
+
+  &:nth-child(3) {
+    position: absolute;
+    top: -90px;
+    left: 55px;
+    transform: rotate(60deg) skew(0deg, -30deg);
+  }
+   */
+  /* 
+  &:first-child > div > div {
+    background-color: red;
+  }
+
+  &:nth-child(2) > div > div {
+    background-color: blue;
+  }
+
+  &:nth-child(3) > div > div {
+    background-color: black;
+  } */
+`;
